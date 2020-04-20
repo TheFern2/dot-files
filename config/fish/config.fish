aee1a43afb83257372bbb44e7a2ab76495d8d0d0
Fernando Balandran <fredz0003@gmail.com>
2020-04-19 20:05:55 -0500

    update

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,1 +1,11 @@
 source ~/.config/fish/.fish_aliases
+
+function fish_prompt
+
+	test $SSH_TTY
+    and printf (set_color red)$USER(set_color brwhite)'@'(set_color yellow)(prompt_hostname)' '
+	test "$USER" = 'root'
+	and echo (set_color red)"#"
+
+    echo -n (set_color cyan)(prompt_pwd) (fish_git_prompt) (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
+end
